69b763757ffeaf58c86b9a7774023820461ea74a
dia006 <manuel.cecconi@hotmail.com>
2021-02-17 21:39:57 +0100

    Failed try to have a tool nuget package

---
+++ FTPFileUpload/Publish.bat
@@ -0,0 +1,2 @@
+dotnet pack FTPFileUpload.csproj 
+dotnet nuget push ./nupkg/FTPFileUpload.1.0.0.nupkg --api-key oy2mjjh4c6cywkcwerraig34ldcfb22e3f6gwwpw3wm4em --source https://api.nuget.org/v3/index.json
